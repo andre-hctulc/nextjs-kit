afe36768d09d1fc46f5c5f95d8566e063504866e
andre-hctulc <andre.tho.44@gmail.com>
2026-07-02 03:32:21 +0200

    chore: update files

--- src/server/server-util.ts
+++ src/server/server-util.ts
@@ -40,7 +40,7 @@ export async function parseFormData(request: NextRequest): Promise<FormData> {
 export type ErrorBoundary<E = unknown, R = any> = (error: E, data: any) => R | Promise<R>;
 
 export function combineErrorBoundaries<E = unknown, R = any>(
-    ...boundaries: ErrorBoundary<E, R | undefined>[]
+    ...boundaries: ErrorBoundary<E, R | undefined | Promise<Awaited<R>>>[]
 ): ErrorBoundary<E, R> {
     return async (err: E, data: any) => {
         for (const boundary of boundaries) {
