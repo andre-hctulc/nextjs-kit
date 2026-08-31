680e57f19b0ab51d3c41e3e707dd3c61bd0ff388
andre-hctulc <andre.tho.44@gmail.com>
2026-08-31 14:23:43 +0200

    chore: update files

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 
 export type ParamValue<T = string> = T | T[] | undefined;
+
 export type RawParams<T = string> = Record<string, ParamValue<T>>;
 
 type SearchParamValue = string | undefined | string[];
